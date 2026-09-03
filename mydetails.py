6e81796088bb3358436460be89596877e637d710
GHAVIN10 <latjorkueth83@gmail.com>
2026-09-03 12:02:00 +0300

    Add files via upload

---
+++ mydetails.py
@@ -0,0 +1,20 @@
+# This is a simple python program that prints my details
+name = "Latjor"
+
+print(name)
+
+age =25
+
+print(age)
+
+My_favourite_language = "python"
+
+print(My_favourite_language)
+
+My_goal = "to become a software engineer"
+
+print(My_goal)
+print(name, age, My_favourite_language, My_goal)
+
+print("Hi,my name is", name)
+print("I am", age, "years old")
